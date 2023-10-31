6136ed3c1bbadbe299a0cd76cbc6dc6aee87d34a
Shubham kumar <106580067+SksOp@users.noreply.github.com>
2023-10-31 18:43:33 +0000

    daily_updatge

--- functions/index.js
+++ functions/index.js
@@ -4,24 +4,42 @@ const { initializeApp } = require("firebase-admin/app");
 initializeApp();
 
 const express = require("express");
-const { DB } = require("./db");
+const { DB, STUDENTS_COLLECTION } = require("./db");
 
 const app = express();
 
 // configure the express app
-app.get("/", async (req, res) => {
-  const data = {
-    first: "Alan",
-    middle: "Mathison",
-    last: "Turing",
-    born: 1912,
-  };
-  const aTuringRef = STUDENTS_COLLECTION.doc("aturing2");
-
-  await aTuringRef.set(data);
-  logger.log("Created document with ID: ", aTuringRef.id);
-  res.send("Hello from Firebase!");
+app.post("/add-data", async (req, res) => {
+  const {
+    name,
+    father_name,
+    mother_name,
+    add_number,
+    add_year,
+    add_class,
+    current_class,
+    section,
+  } = req.body;
+  const aTuringRef = STUDENTS_COLLECTION.doc("aturing");
+  try {
+    await aTuringRef.set(
+      {
+        name,
+        father_name,
+        mother_name,
+        add_number,
+        add_year,
+        current_class,
+        add_class,
+        section,
+      },
+      { merge: true }
+    );
+    res.status(200).json({ message: "Document updated successfully." });
+  } catch (err) {
+    console.log("Error Created on Updating Document", err);
+    res.status(500).json({ error: err });
+  }
 });
-
 // export the express app as an HTTP function
 exports.api = onRequest(app);
