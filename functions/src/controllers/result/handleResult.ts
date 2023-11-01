6abe9560a234371d6ee5518b01cf39b4637d7d23
shivam <106580067+SksOp@users.noreply.github.com>
2023-11-01 18:52:24 +0000

    minor fixes

--- functions/src/controllers/result/handleResult.ts
+++ functions/src/controllers/result/handleResult.ts
@@ -1,4 +1,4 @@
 import { Request, Response } from "express";
 import { RESULTS_COLLECTION } from "../../db";
 
-async function handleResult(req: Request, res: Response) {}
+export async function handleResult(req: Request, res: Response) {}
