645ca2e9383f413ed2c57de5448c0a5022350f72
Shubham kumar <106580067+SksOp@users.noreply.github.com>
2023-11-08 12:51:23 +0000

    added batch config

--- app/src/components/adminBoard.tsx
+++ app/src/components/adminBoard.tsx
@@ -1,69 +1,129 @@
-import React from "react";
-import { Button, Typography, Box, Container, Grid } from "@mui/material";
-import { useNavigate } from "react-router-dom"; // assuming you are using react-router for navigation
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Typography,
+  Box,
+  Container,
+  Card,
+  CardContent,
+  CardActions,
+  Grid,
+  Stack,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import { paths } from "src/router";
-import SchoolIcon from "@mui/icons-material/School"; // Importing icons from Material UI
-import AssessmentIcon from "@mui/icons-material/Assessment"; // Importing icons from Material UI
+import SchoolIcon from "@mui/icons-material/School";
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import dayjs from "dayjs";
+import { getCurrentBatch } from "src/utils/management";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import { useSnackbar } from "notistack";
 
 function AdminBoard() {
   const navigate = useNavigate();
+  const [currentBatch, setCurrentBatch] = useState(null);
+  const [canChangeBatch, setCanChangeBatch] = useState(false);
+  const { enqueueSnackbar } = useSnackbar();
 
-  // Function to navigate to the Manage Students page
+  useEffect(() => {
+    async function fetchBatch() {
+      const batch = await getCurrentBatch();
+      setCurrentBatch(batch);
+      const month = dayjs().month();
+      // January is 0 in dayjs, so March is 2 and April is 3
+      if (month === 2 || month === 3) {
+        setCanChangeBatch(true);
+      }
+    }
+
+    fetchBatch();
+  }, []);
+
   const goToManageStudents = () => {
-    navigate(paths.dashboard.student.root); // Update this with your actual route
+    navigate(paths.dashboard.student.root);
   };
 
-  // Function to navigate to the Result Dashboard page
   const goToResultDashboard = () => {
-    navigate(paths.dashboard.result.root); // Update this with your actual route
+    navigate(paths.dashboard.result.root);
+  };
+
+  const changeBatch = () => {
+    enqueueSnackbar(
+      "Changing batch is not supported yet. Please contact your developer",
+      {
+        variant: "warning",
+      }
+    );
   };
 
   return (
-    <Container component="main">
-      <Box
-        sx={{
-          flexGrow: 1,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          padding: 3,
-          mt: 4,
-        }}
-      >
-        <Typography component="h1" variant="h4" gutterBottom>
-          Admin Dashboard
-        </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          Welcome to the administration panel. Here you can oversee and manage
-          critical aspects of the educational process.
-        </Typography>
+    <Container component="main" maxWidth="md">
+      <Card sx={{ mt: 4 }}>
+        <CardContent>
+          <Typography component="h1" variant="h4" gutterBottom>
+            Admin Dashboard
+          </Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            Welcome to the administration panel. Here you can oversee and manage
+            critical aspects of the educational process.
+          </Typography>
 
-        <Grid container spacing={2} sx={{ marginTop: 2 }}>
-          <Grid item xs={12} sm={6}>
+          {currentBatch && (
+            <Box sx={{ my: 2, display: "flex", alignItems: "center" }}>
+              <BatchStatusIndicator batch={currentBatch} />
+            </Box>
+          )}
+
+          <Grid container spacing={2} sx={{ marginTop: 2 }}>
+            <Grid item xs={12} sm={6}>
+              <Button
+                variant="contained"
+                fullWidth
+                startIcon={<SchoolIcon />}
+                onClick={goToManageStudents}
+                sx={{ py: 2 }}
+              >
+                Manage Students
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Button
+                variant="contained"
+                fullWidth
+                startIcon={<AssessmentIcon />}
+                onClick={goToResultDashboard}
+                sx={{ py: 2 }}
+              >
+                Result Dashboard
+              </Button>
+            </Grid>
+          </Grid>
+          {canChangeBatch && (
             <Button
+              sx={{ mt: 2 }}
               variant="outlined"
               fullWidth
-              startIcon={<SchoolIcon />}
-              onClick={goToManageStudents}
-              sx={{ py: 2 }}
+              color="primary"
+              onClick={changeBatch}
             >
-              Manage Students
+              Change Batch
             </Button>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Button
-              variant="outlined"
-              fullWidth
-              startIcon={<AssessmentIcon />}
-              onClick={goToResultDashboard}
-              sx={{ py: 2 }}
-            >
-              Result Dashboard
-            </Button>
-          </Grid>
-        </Grid>
-      </Box>
+          )}
+        </CardContent>
+      </Card>
     </Container>
   );
 }
+
 export default AdminBoard;
+
+const BatchStatusIndicator = ({ batch }: { batch: string }) => {
+  return (
+    <Stack direction="row" spacing={1} alignItems="center">
+      <FiberManualRecordIcon sx={{ color: "success.main" }} />
+      <Typography variant="h6" component="p">
+        Current Batch: <strong>{batch}</strong>
+      </Typography>
+    </Stack>
+  );
+};
---
+++ app/src/utils/management.ts
@@ -0,0 +1,12 @@
+import { doc, getDoc } from "firebase/firestore";
+import { managementCollection } from "src/firebase";
+
+export async function getCurrentBatch() {
+  const managementDoc = await getDoc(doc(managementCollection, "management"));
+  const management = managementDoc.data();
+  const currentBatch = management?.currentBatch;
+  if (!currentBatch) {
+    return null;
+  }
+  return currentBatch;
+}
